e2cc53ec7d23a4611272800588e18b607700a5b7
Daniel Taylor <32105556+Ambulato@users.noreply.github.com>
2018-08-11 11:11:33 -0500

    Add files via upload
    MCSAP - MetaCountSystemArbitraryPrecision: a tool for creating a (fast) count system with an arbitrary number base, but without all the junk BaseN has in it.
    
    The only other major change here is the addition of a times table to speed up multiplication.

---
+++ Binary.java
@@ -0,0 +1,24 @@
+
+/**
+ * 
+ * 
+ * @author Daniel Taylor (DTIII)
+ * @version .._-
+ */
+public class Binary extends CountSystem
+{
+    static final String BASE = "2";
+    static final int BASE_INT = 2;
+    
+    public Binary()
+    {
+        super(BASE);
+    }
+    
+    void initAlphabet()
+    {
+        alpha = new char[BASE_INT];
+        alpha[0] = '0';
+        alpha[1] = '1';
+    }
+}
